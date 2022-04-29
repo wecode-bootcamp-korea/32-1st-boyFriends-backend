55668102666c4687204ecd4d52c19d0e83e17c6a
BDjaekwanee <jaekwanee@gmail.com>
2022-04-29 18:02:47 +0900

    feat: Create login_decorator

--- core/utils.py
+++ core/utils.py
@@ -11,11 +11,17 @@ def login_decorator(func) :
             payload = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITMS)
             request.user = User.objects.get(email=payload['email'])
 
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE' : 'INVALID_USER'}, status=401)
+
         except jwt.exceptions.DecodeError :
-            return JsonResponse({"MESSAGE": "INVALID_TOKEN"}, status=401)
+            return JsonResponse({'MESSAGE': 'INVALID_TOKEN'}, status=401)
         
         except jwt.ExpiredSignatureError :
-            return JsonResponse({'message':"EXPIRED_TOKEN"}, status=401)
+            return JsonResponse({'message':'EXPIRED_TOKEN'}, status=401)
+
+        except jwt.InvalidSignatureError:
+            return JsonResponse({'message' : 'invalid_signature'}, status=401)
         
         return func(self, request, *args, **kwrags)
     
