ef850c76c90f4ed4c2dd4802ee2b14cdbe6f2d54
jiggy <jiggyjiggy0323@gmail.com>
2022-05-01 22:48:53 +0900

    feat: Add post, delete

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http     import JsonResponse
 from django.views    import View
 
@@ -7,6 +9,25 @@ from payments.models import Cart
 from core.utils      import login_decorator
 
 class CartView(View):
+    @login_decorator
+    def post(self, request):
+        data = json.loads(request.body)
+
+        user_id    = request.user
+        product_id = data["product_id"]
+        count      = data["count"]
+        check_box  = data["isChecked"]
+
+        cart, created  = Cart.objects.get_or_creat(
+            user_id    = user_id,
+            product_id = product_id,
+            count      = count,
+            check_box  = check_box
+        )
+        if not created:
+            cart.save()
+        return JsonResponse({"results": results}, status=201)
+
     @login_decorator
     def get(self, request):
         carts      = Cart.object.filter(user_id=request.user)
@@ -21,7 +42,19 @@ class CartView(View):
                 "size"     : [(sizestock.size.size for sizestock in sizestocks) if sizestocks.size else "단품"],
                 "sale"     : cart.product.discount,
                 "stock"    : [sizestock.stock.stock for sizestock in sizestocks],
-                "isChecked": cart.check_box
+                "isChecked": cart.check_box,
+                "cart_id"  : cart.id
             } for cart in carts
         ]
         return JsonResponse({"results": results}, status=200)
+
+    @login_decorator
+    def delete(self, request):
+        cart_ids = request.GET.getlist('cart_ids', None)
+        user_id  = request.user
+
+        if cart_ids is None:
+            return JsonResponse({'message': 'CART_EMPTY'}, status=404)
+
+        Cart.objects.filter(id__in=cart_ids, user_id=user_id).delete()
+        return JsonResponse({'message': 'CART_DELETED'}, status=204)
