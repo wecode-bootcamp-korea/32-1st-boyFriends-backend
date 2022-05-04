93197befb78b83c1145310ca548ebea759979723
jiggy <jiggyjiggy0323@gmail.com>
2022-05-04 10:57:02 +0900

    refactor: Apply identification_decorator for user name

--- core/views.py
+++ core/views.py
@@ -2,18 +2,22 @@ from django.http     import JsonResponse
 from django.views    import View
 
 from products.models import MainCategory
+from core.utils      import identification_decorator
+
 
 class NavView(View):
+    @identification_decorator
     def get(self, request):
         main_categories = MainCategory.objects.all()
 
         results = [
             {
-                "menuName"   : main_category.main,
-                "menuLink"   : main_category.id,
-                "sub" : [
+                "userName": request.user.name if request.user else None,
+                "menuName": main_category.main,
+                "menuLink": main_category.id,
+                "sub": [
                     {
-                        "subMenu": main_category.category_set.all()[i].sub,
+                        "subMenu"    : main_category.category_set.all()[i].sub,
                         "subMenuLink": main_category.category_set.all()[i].id
                     } for i in range(len(main_category.category_set.all()))
                 ]
