019e74e3984754705a3cc46659097a6a2e7a6921
jiggy <jiggyjiggy0323@gmail.com>
2022-05-04 11:04:13 +0900

    refactor: Fix format; user name

--- core/views.py
+++ core/views.py
@@ -11,16 +11,16 @@ class NavView(View):
         main_categories = MainCategory.objects.all()
 
         results = [
-            {
-                "userName": request.user.name if request.user else None,
-                "menuName": main_category.main,
-                "menuLink": main_category.id,
-                "sub": [
+            [{ "userName" : request.user.name if request.user else None}],
+            [{
+                "menuName" : main_category.main,
+                "menuLink" : main_category.id,
+                "sub" : [
                     {
-                        "subMenu"    : main_category.category_set.all()[i].sub,
+                        "subMenu": main_category.category_set.all()[i].sub,
                         "subMenuLink": main_category.category_set.all()[i].id
                     } for i in range(len(main_category.category_set.all()))
                 ]
-            } for main_category in main_categories
+            } for main_category in main_categories]
         ]
         return JsonResponse({"results": results}, status=200)
