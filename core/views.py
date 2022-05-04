116b9f67e27eed4dd9f5e4d37f12e793ed085c59
jiggy <jiggyjiggy0323@gmail.com>
2022-05-04 16:31:57 +0900

    refactor: Delete decorator in Nav view

--- core/views.py
+++ core/views.py
@@ -2,21 +2,13 @@ from django.http     import JsonResponse
 from django.views    import View
 
 from products.models import MainCategory
-from core.utils      import identification_decorator
 
 
 class NavView(View):
-    @identification_decorator
     def get(self, request):
         main_categories = MainCategory.objects.all()
 
         results = [
-            [
-                {
-                    "userName": request.user.name if request.user else None
-                }
-            ],
-            [
                 {
                     "menuName": main_category.main,
                     "menuLink": main_category.id,
@@ -27,6 +19,5 @@ class NavView(View):
                         } for sub_category in main_category.category_set.all()
                     ]
                 } for main_category in main_categories
-            ]
         ]
         return JsonResponse({"results": results}, status=200)
