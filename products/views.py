b96505d55e1df1fbcd4e5dfc4fe9f8b673b66813
jiggy <jiggyjiggy0323@gmail.com>
2022-05-05 13:28:41 +0900

    refactor: Fix results value statement, Fix annotate format, Fix offset and limit; price ordering doesn't work

--- products/views.py
+++ products/views.py
@@ -6,10 +6,10 @@ from django.db.models import (
                           Case,
                           When,
                           Avg,
-                          Count
+                          Count,
                       )
 
-from products.models  import Product, SizeStock, Category
+from products.models  import Product, Category
 
 
 class ProductsListView(View):
@@ -23,49 +23,58 @@ class ProductsListView(View):
 
         THUMBNAIL = 2
 
-        size_stock = SizeStock.objects
-
         q = Q()
+        page_title = "모든상품"
 
         if search:
             q &= Q(name__icontains=search)
+            page_title = "검색결과"
 
         if main_category_id:
             q &= Q(category_id__main_category_id=main_category_id)
+            page_title = Category.objects.filter(main_category_id=main_category_id).first().main_category.main
 
         if category_id:
             q &= Q(category_id__id=category_id)
+            page_title = Category.objects.get(id=category_id).sub
 
         order = {
-            "min_price": "discount_price",
-            "max_price": "-discount_price",
-            "high_rating": "-review__rating",
-            "reviews": "-reviews",
-            "best": "-best",
-            "new": "-new"
+            "min_price"  : "discount_price",
+            "max_price"  : "-discount_price",
+            "high_rating": "-total_rating",
+            "reviews"    : "-reviews",
+            "best"       : "-best",
+            "new"        : "-new"
         }
 
-        products = Product.objects.annotate(discount_price=Case(When(discount=True, then=F("price") * (100 - F("discount") / 100)), default=F("price")))\
+        products = Product.objects\
+            .annotate(discount_price=(F("price") * ((100 - F("discount") / 100))))\
             .annotate(reviews=Count("review"))\
+            .annotate(total_rating=Avg("review__rating"))\
             .annotate(best=Case(When(product_status__status="Best", then=True), default=False))\
-            .annotate(new=Case(When(product_status__status="New", then=True), default=False)) \
-            .filter(q).order_by(order[order_condition])[offset:limit]
+            .annotate(new=Case(When(product_status__status="New", then=True), default=False))\
+            .filter(q)\
+            .order_by(order[order_condition])\
+            [offset:offset+limit]
 
         results = [
             {
-                "id"            : product.id,
-                "name"          : product.name,
-                "price"         : product.price,
-                "discount"      : product.discount,
-                "discount_price": ((product.price) * (100 - (product.discount))) // 100 if product.discount else product.price,
-                "stock"         : size_stock.get(id=product.id).stock.stock,
-                "is_soldout"    : "is_soldout" if size_stock.get(id=product.id).stock.stock == 0 else "on_stock",
-                "img"           : product.image_set.get(image_type_id=THUMBNAIL).image_urls if product.image_set.filter(image_type_id=THUMBNAIL) else None,
-                "reviewCount"   : product.review_set.aggregate(Count('id'))["id__count"] if product.review_set.exists() else None,
-                "reputation"    : product.review_set.aggregate(Avg("rating"))["rating__avg"] if product.review_set.exists() else None,
-                "descript"      : product.caution.description if product.caution else None,
-                "status"        : product.product_status.status if product.product_status else None
-            } for product in products
+                "pageTitle": page_title
+            },
+            [
+                {
+                    "id"            : product.id,
+                    "name"          : product.name,
+                    "price"         : product.price,
+                    "discount"      : product.discount,
+                    "discount_price": ((product.price) * (100 - (product.discount))) // 100,
+                    "stock"         : sum([size_stock.stock.stock for size_stock in product.sizestock_set.filter()]),
+                    "img"           : product.image_set.get(image_type_id=THUMBNAIL).image_urls if product.image_set.filter(image_type_id=THUMBNAIL) else None,
+                    "reviewCount"   : product.reviews if product.reviews else None,
+                    "reputation"    : round(product.total_rating, 1) if product.total_rating else None,
+                    "descript"      : product.caution.description if product.caution else None,
+                    "status"        : product.product_status.status if product.product_status else None
+                } for product in products
+            ]
         ]
-
         return JsonResponse({"results": results}, status=200)
